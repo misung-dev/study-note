d245d5563696bf85945210b125b63ef1cf495c0c
misung.dev <misung.dev@gmail.com>
2023-04-13 00:30:13 +0900

    Del: 파일 삭제

--- Python/n-sum.py
+++ Python/n-sum.py
@@ -1,8 +1,9 @@
 # while 루프를 이용하여 자연수를 더하는 프로그램
-count = 1
+count = 0
 sum = 0
 limit = 1000
-while sum + count < limit:
+while sum < limit:
     sum = sum + count
     count = count + 1
-print("1부터", count - 1, "까지의 합계:", sum)
+print("1부터", count, "까지의 합계:", sum)
+
