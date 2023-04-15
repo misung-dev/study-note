9e4c861afd24786392f5eb599362f2cf846331c1
misung.dev <misung.dev@gmail.com>
2023-04-15 22:12:19 +0900

    Fix: 일부 코드 수정

--- Python/sum_average.py
+++ Python/sum_average.py
@@ -1,12 +1,24 @@
+count = 0
 sum = 0
-count = 1
 average = 0
-score = print("점수 입력")
-while score != 0:
+
+print("점수 입력")
+
+while True:
     score = int(input())
-    if score >= 1:
+
+    if score == 0:
+        break
+
+    elif score < 0:
+        continue
+
+    else:
         sum += score
         count += 1
-        average = sum / count
+
+
+if count > 0:
+    average = sum / count
         
-print("합:", sum, "개수:", count - 1, "평균: %.2f" %average)
+print("합:", sum, "개수:", count, "평균: %.2f" %average)
