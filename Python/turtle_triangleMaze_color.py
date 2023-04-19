46fe253e3fe0919820586c1c3a9121b7dc401227
misung.dev <misung.dev@gmail.com>
2023-04-19 15:10:06 +0900

    Fix: 색상 순환 코드 추가

--- Python/turtle_triangleMaze_color.py
+++ Python/turtle_triangleMaze_color.py
@@ -2,10 +2,10 @@ import turtle
 
 turtle.setup(width = 500, height = 500)
 t = turtle.Pen()
-t.pencolor("blue")
+colors = ["red", "blue", "green", "black"]
 t.width(3)
 
-for i in range(0, 301, 8):
+for i in range(0, 200, 5):
     t.forward(i)
     t.left(120)
-
+    t.pencolor(colors[i%4])
