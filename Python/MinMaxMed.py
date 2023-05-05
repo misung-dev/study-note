8e124efa15c53386b17bb4eb55518dd5207324cc
misung.dev <misung.dev@gmail.com>
2023-05-06 02:51:33 +0900

    Feat: 리스트의 최대, 최소, 중간값 찾기 코드

---
+++ Python/MinMaxMed.py
@@ -0,0 +1,24 @@
+number = []
+
+while True :
+    print("Enter numbers. (To finish press 'Enter' key)")
+    n = input()
+    if len(n) == 0 :
+        break
+    number.append(float(n))
+
+number.sort()
+number_len = len(number)
+midIndex = number_len // 2
+
+if number_len % 2 == 0:
+    number_median = (number[midIndex - 1] + number[midIndex]) / 2
+else :
+    number_median = number[midIndex]
+
+print("You entered")
+print(number)
+print("")
+print("max :", max(number))
+print("min :", min(number))
+print(f"median : {number_median:.2f}")
