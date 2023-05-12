cbaa1d006869dcf0bf1ab2aef202c8fbae322b57
misung.dev <misung.dev@gmail.com>
2023-05-13 00:56:07 +0900

    Feat: 영한 사전 프로그램 코드 추가

--- Python/wordDict.py
+++ Python/wordDict.py
@@ -8,7 +8,7 @@ while True:
     print('\t' + '2. Delete words')
     print('\t' + '3. Print dictionary')
     print('\t' + '4. Look up words')
-    print('\t' + '5. vocabulary test')
+    print('\t' + '5. Vocabulary test')
     print('\t' + '6. Exit')
     print('=' * 49)
     menu = int(input('Select a menu: '))
@@ -38,13 +38,33 @@ while True:
         print()
 
     elif menu == 3:
-        print('Sookmyung dictionary:', dictionary)
-        for words in dictionary:
-                print('   ' + words)
+        print('Sookmyung dictionary:')
+        for key, value in sorted(dictionary.items()):
+            print('%20s  %20s' % (key, value))
+        print()
+            
+    elif menu == 4:
+        print('Enter words to look up (Press ENTER to finish)')
+        while True:
+            key = input('word: ')
+            if len(key) == 0:
+                break
+            elif key in dictionary:
+                print(f'{dictionary[key]}')
+            else:
+                print('  ', f"'{key}' not found")
         print()
 
+    elif menu == 5:
+        score = 0
+        for key in dictionary:
+            answer = input(f"meaning of '{key}': ")
+            if answer == value:
+                score += 1
+        print(f"*** {score} correct answers out of {len(dictionary)} ***\n")
+            
     elif menu == 6:
         break
 
     else:
-        print('You entered a wrong menu!!'+ '\n')
+        print('You entered a wrong menu!!')
