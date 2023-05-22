c1a92c72ad3649b7dcf836d2615a74a9e79246ac
misung.dev <misung.dev@gmail.com>
2023-05-22 20:22:29 +0900

    Refactor: 근로소득세 계산 코드

--- Python/income-tax1.py
+++ Python/income-tax1.py
@@ -1,16 +1,25 @@
 def tax1(income):
-    if income <= 12000000:
-        tax1 = income * 0.06
-    elif income <= 46000000:
-        tax1 = 720000 + ((income-12000000) * 0.15)
-    elif income <= 88000000:
-        tax1 = 5820000 + ((income-46000000) * 0.24)
-    elif income <= 300000000:
-        tax1 = 15900000 + ((income-88000000) * 0.35)
-    else:
-        tax1 = 90100000 + ((income-300000000) * 0.38)
-    return tax1
+  taxrates = [(300000000, 0.38), (88000000, 0.35), (46000000, 0.24),
+              (12000000, 0.15), (0, 0.06)]
+  sortedTaxrates = sorted(taxrates)
+  baseTax = {0: 0}
 
-incomes = [5000000, 12000000, 30000000, 46000000, 60000000, 88000000, 200000000, 300000000, 400000000]
+  [prevBaseIncome, prevTaxrate] = sortedTaxrates[0]
+
+  for (baseIncome, taxrate) in sortedTaxrates:
+    baseTax[baseIncome] = int(baseTax[prevBaseIncome] +
+                              (baseIncome - prevBaseIncome) * prevTaxrate)
+    prevBaseIncome = baseIncome
+    prevTaxrate = taxrate
+
+  for (baseIncome, taxrate) in taxrates:
+    if income > baseIncome:
+      return baseTax[baseIncome] + (income - baseIncome) * taxrate
+
+
+incomes = [5000000, 12000000, 30000000, 46000000, 60000000, 88000000, 200000000,
+  300000000, 400000000
+]
+
 for income in incomes:
-    print("소득: %d \t 세금: %d" % (income, tax1(income)))
+  print("소득: %d \t 세금: %d" % (income, tax1(income)))
