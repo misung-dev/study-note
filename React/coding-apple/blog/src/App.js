242f567d7270267a98ee488dd5b4d3475706968e
misung.dev <misung.dev@gmail.com>
2023-08-08 17:25:43 +0900

    Feat: 버튼에 기능개발을 해보자 & 리액트 state변경하는 법

--- React/coding-apple/blog/src/App.js
+++ React/coding-apple/blog/src/App.js
@@ -5,15 +5,26 @@ import { useState } from "react";
 function App() {
 	let post = "Contents";
 	let [글제목, b] = useState(["여자 코트 추천", "강남 우동 맛집", "파이썬 독학"]);
+	let [따봉, 따봉변경] = useState(0);
 
 	return (
 		<div className="App">
 			<div className="black-nav">
-				<h4 style={{ color: "red", fontSize: "16px" }}>ReactBlog</h4>
+				<h4 style={{ fontSize: "16px" }}>ReactBlog</h4>
 			</div>
 			<h4>{post}</h4>
 			<div className="list">
-				<h4>{글제목[0]}</h4>
+				<h4>
+					{글제목[0]}{" "}
+					<span
+						onClick={() => {
+							따봉변경(따봉 + 1);
+						}}
+					>
+						👍
+					</span>{" "}
+					{따봉}
+				</h4>
 				<p>2월 17일 발행</p>
 			</div>
 			<div className="list">
