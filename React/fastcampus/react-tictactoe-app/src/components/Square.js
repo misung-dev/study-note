8464225973f794d958006a1330c1a21c352073a8
misung.dev <misung.dev@gmail.com>
2024-01-26 22:48:46 +0900

    Feat: 리액트 Hooks를 이용해서 TicTacToe 앱을 함수형 컴포넌트로 바꾸기

--- React/fastcampus/react-tictactoe-app/src/components/Square.js
+++ React/fastcampus/react-tictactoe-app/src/components/Square.js
@@ -1,17 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Square.css";
 
-export default class Square extends Component {
-	render() {
-		return (
-			<button
-				className="square"
-				onClick={() => {
-					this.props.onClick(); // state 변경하기
-				}}
-			>
-				{this.props.value}
-			</button> // 어떤 것을 기억해야 할때는 state를 이용
-		);
-	}
-}
+const Square = ({ onClick, value }) => {
+	return (
+		<button className="square" onClick={onClick}>
+			{value}
+		</button> // 어떤 것을 기억해야 할때는 state를 이용
+	);
+};
+
+export default Square;
