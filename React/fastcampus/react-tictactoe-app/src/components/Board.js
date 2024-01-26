27454a3fb25528afd6fb36d8e9ad5ffc3781371d
misung.dev <misung.dev@gmail.com>
2024-01-27 00:43:21 +0900

    Feat: 승자 결정하기

--- React/fastcampus/react-tictactoe-app/src/components/Board.js
+++ React/fastcampus/react-tictactoe-app/src/components/Board.js
@@ -6,10 +6,42 @@ const Board = () => {
 	const [squares, setSquares] = useState(Array(9).fill(null));
 	const [xIsNext, setXISNext] = useState(true);
 
-	const status = `Next player: ${xIsNext ? "X" : "O"}`;
+	const calculateWinner = (squares) => {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		for (let index = 0; index < lines.length; index++) {
+			const [a, b, c] = lines[index];
+			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				return squares[a];
+			}
+		}
+		return null;
+	};
+
+	const winner = calculateWinner(squares);
+	let status;
+	if (winner) {
+		status = "Winner: " + winner;
+	} else {
+		status = `Next player: ${xIsNext ? "X" : "O"}`;
+	}
 
 	const handleClick = (i) => {
 		const newSquares = squares.slice();
+
+		// 승자가 결정되면 게임이 종료와 동시에 더 이상 클릭이 안됨.
+		if (calculateWinner(newSquares) || newSquares[i]) {
+			return;
+		}
+
 		newSquares[i] = xIsNext ? "X" : "O";
 		setSquares(newSquares); // setter를 이용함
 		setXISNext((previousState) => !previousState);
